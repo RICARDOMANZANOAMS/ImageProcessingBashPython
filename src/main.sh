7298820786093590ca3812d1f6f4053dfdcfc91a
Ricardo Manzano <ricardoamsrams@gmail.com>
2025-03-19 21:38:18 -0400

    Add loop to iterate though all json configuration file

---
+++ src/main.sh
@@ -0,0 +1,24 @@
+
+export PROJECT_PATH="/RICARDO/personal/ImageProcessingBashPython"  #Define project path
+WSL_PROJECT_PATH=$(wslpath "$PROJECT_PATH")  #Transform path to wsl path 
+echo $WSL_PROJECT_PATH
+
+jsonFile=$WSL_PROJECT_PATH/config/config.json  #Path to config file
+
+jq -c '.[]' $jsonFile | while read -r item; do
+    echo $item | jq -c '.[]' | while read -r item2; do
+        echo $item2
+        keys=$(echo "$item2" | jq -r 'keys_unsorted[]')
+        echo $keys
+        for key in $keys; do
+            enabled_value=$(echo "$item2" | jq -r --arg key "$key" '.[$key].enabled')
+            echo $enabled_value
+            if [ "$enabled_value" = "true" ]; then
+                echo "true value"
+            else
+                echo "false value"
+            fi
+        done
+       
+    done
+done
